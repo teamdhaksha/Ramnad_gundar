22d98a8f5962a6c2a459d2f14a2f490cf9c1ddd7
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-01-09 15:24:08 +0530

    Add files via upload

---
+++ data/Encroachment_8.js
@@ -0,0 +1,24 @@
+var json_Encroachment_8 = {
+"type": "FeatureCollection",
+"name": "Encroachment_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Village": "Kallikudi", "Taluk": "Paramakudi (SC)" }, "geometry": { "type": "Point", "coordinates": [ 78.44926929, 9.58248121 ] } },
+{ "type": "Feature", "properties": { "Village": "Manaloor", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.4554926, 9.43098782 ] } },
+{ "type": "Feature", "properties": { "Village": "Anaiyur", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.42588529, 9.38030489 ] } },
+{ "type": "Feature", "properties": { "Village": "Anaiyur", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.42535649, 9.37794466 ] } },
+{ "type": "Feature", "properties": { "Village": "Pakkuvetti", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.40251044, 9.36486915 ] } },
+{ "type": "Feature", "properties": { "Village": "Pakkuvetti", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.39810687, 9.37198668 ] } },
+{ "type": "Feature", "properties": { "Village": "Kumudi", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.36466449, 9.42908922 ] } },
+{ "type": "Feature", "properties": { "Village": "Kallikulam", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.42585276, 9.31302705 ] } },
+{ "type": "Feature", "properties": { "Village": "Kallikulam", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.42296532, 9.31097233 ] } },
+{ "type": "Feature", "properties": { "Village": "O.Karisalkulam", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.42298774, 9.30669946 ] } },
+{ "type": "Feature", "properties": { "Village": "Kovilangulam", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.42690251, 9.30521261 ] } },
+{ "type": "Feature", "properties": { "Village": "Kovilangulam", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.42945059, 9.30261574 ] } },
+{ "type": "Feature", "properties": { "Village": "Kovilangulam", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.43050708, 9.30212062 ] } },
+{ "type": "Feature", "properties": { "Village": "Kovilangulam", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.43566156, 9.3004566 ] } },
+{ "type": "Feature", "properties": { "Village": "Kovilangulam", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.43624327, 9.30026017 ] } },
+{ "type": "Feature", "properties": { "Village": "Komboothi", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.44515441, 9.27043109 ] } },
+{ "type": "Feature", "properties": { "Village": "Archanaipagam Usilangulam", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.48421223, 9.1859596 ] } }
+]
+}
---
+++ data/Mining_7.js
@@ -0,0 +1,14 @@
+var json_Mining_7 = {
+"type": "FeatureCollection",
+"name": "Mining_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Village": "Kallikudi", "Taluk": "Paramakudi (SC)" }, "geometry": { "type": "Point", "coordinates": [ 78.45207364, 9.59545283 ] } },
+{ "type": "Feature", "properties": { "Village": "Perianaikkulam", "Taluk": "Paramakudi (SC)" }, "geometry": { "type": "Point", "coordinates": [ 78.44878178, 9.52447284 ] } },
+{ "type": "Feature", "properties": { "Village": "Perianaikkulam", "Taluk": "Paramakudi (SC)" }, "geometry": { "type": "Point", "coordinates": [ 78.45144771, 9.52084911 ] } },
+{ "type": "Feature", "properties": { "Village": "Perianaikkulam", "Taluk": "Paramakudi (SC)" }, "geometry": { "type": "Point", "coordinates": [ 78.45817846, 9.51673087 ] } },
+{ "type": "Feature", "properties": { "Village": "Melakodumalur", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.46169203, 9.48635683 ] } },
+{ "type": "Feature", "properties": { "Village": "Manaloor", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.44855207, 9.41142223 ] } },
+{ "type": "Feature", "properties": { "Village": "Anaiyur", "Taluk": "Mudhukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.41964098, 9.39361372 ] } }
+]
+}
